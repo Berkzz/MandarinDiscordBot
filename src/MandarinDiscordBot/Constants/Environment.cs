daeade2c20cde47916feb7d4aeba4f9c80a52f80
Максим Попов <mpopov@htc-cs.ru>
2022-12-28 18:49:56 +0400

    Подключение бота по ключу

---
+++ src/MandarinDiscordBot/Constants/Environment.cs
@@ -0,0 +1,6 @@
+﻿namespace MandarinDiscordBot.Constants;
+
+public static class EnvironmentConstants
+{
+    public const string KeyName = "MandarinDiscordBotKey";
+}
--- src/MandarinDiscordBot/Program.cs
+++ src/MandarinDiscordBot/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using MandarinDiscordBot.Constants;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        //_client.Log += Log;
+
+        var token = Environment.GetEnvironmentVariable(EnvironmentConstants.KeyName);
+
+        if(token == null) 
+        {
+            return;
+        }
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
